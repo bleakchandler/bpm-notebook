85d412170f09f4e0c7713aa5b458ff4d988c391d
Josh Frank <eurosewusa@gmail.com>
2021-01-05 15:48:09 -0500

    wip9 'carbonara' - began exploring how to create a run loop, fine-tuned Interface#create_or_log_in_user, Interface#create_new_user, Interface#main_menu, Interface#create_new_setlist

--- app/bpm_notebook.rb
+++ app/bpm_notebook.rb
@@ -3,8 +3,21 @@ class BPMNotebook
     def run
       interface = Interface.new
       interface.welcome
-      user = interface.create_or_log_in_user
+      user_id = interface.create_or_log_in_user
+      choice = nil
+      while choice != :goodbye
+        choice = interface.main_menu( User.find( user_id ) )
+        if choice == :new_setlist
+          interface.create_new_setlist( User.find( user_id ) )
+        else
+          interface.manage_setlist( choice.to_s, User.find( user_id ) )
+        end
+      end
+      interface.goodbye
+    end
 
+    def main_loop
+      #
     end
 
 end
--- app/models/interface.rb
+++ app/models/interface.rb
@@ -7,11 +7,13 @@ class Interface
     end
 
     def welcome
+        system "clear"
         puts "Awesome Welcome Screen"
         self.prompt.keypress("Press any key to continue")
     end
 
     def create_or_log_in_user
+        system "clear"
         user_logging_in = self.prompt.ask("Enter Username")
         user_logged_in = nil
         if User.where(name: user_logging_in).empty?
@@ -27,10 +29,10 @@ class Interface
           puts "#{ user_logged_in.name } has logged in successfully"
           self.prompt.keypress("Press any key to continue")
         end
-        user_logged_in
-      end
+        user_logged_in.id
+    end
 
-      def create_new_user(new_username)
+    def create_new_user(new_username)
         new_user_arguments = {name: new_username}
         new_user_arguments[:spotify_username] = self.prompt.ask("Enter your Spotify username")
         new_user_arguments[:password] = self.prompt.mask("Enter a password")
@@ -39,30 +41,41 @@ class Interface
           puts "Passwords do not match, please try again"
           password_confirmation = self.prompt.mask("Confirm your password")
         end
-        User.create(new_user_arguments)
-      end
-
-
-
-    def main_menu(user)
-        #all setlists
-        #create new setlists
-        menu_hash = user.setlists.map{|setlist| [setlist.name, manage_setlist(setlist, self)] }.to_h
-        menu_hash["(new setlist)".to_sym] = create_new_setlist(user)
-        self.prompt.select("Choose a setlist or create a new setlist:", menu_hash)
+        User.create(new_user_arguments).id
     end
 
-    def manage_setlist(setlist, user)
-
+    def main_menu( user )
+        # Main menu:
+        # - list of all setlists
+        # - (create new setlist)
+        # - (quit)
+        system "clear"
+        menu_hash = user.setlists.map{ | setlist | [ setlist.name, setlist.name.to_sym ] }.to_h
+        menu_hash.store( "(new setlist)".to_sym, :new_setlist )
+        menu_hash.store( "(quit)".to_sym, :goodbye )
+        self.prompt.select("Choose a setlist or create a new setlist:", menu_hash )
     end
 
+    def manage_setlist( setlist_name, user )
+        #
+    end
 
-    def create_new_setlist(user)
-
+    def create_new_setlist( user )
+        system "clear"
+        new_setlist_name = self.prompt.ask( "Enter a name for your new setlist" )
+        new_setlist_tempo = self.prompt.ask( "Enter a tempo for your new setlist", convert: :float )
+        Setlist.create( name: new_setlist_name, tempo: new_setlist_tempo, user_id: user.id )
+        puts "Setlist '#{ new_setlist_name }' successfully created!"
+        self.prompt.keypress("Press any key to continue")
     end
 
     def setlist_edit_menu
         #
+    end
+
+    def goodbye
+        puts "Awesome Goodbye screen"
+        puts "Successfully logged out! Goodbye!"
     end
 
 end
